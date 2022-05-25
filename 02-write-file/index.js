dda166f67ec581c90b6d51de302b96f875cbf30b
MikhailPakaliuk <mr.dyurilen@mail.ru>
2022-05-25 14:28:06 +0300

    fix: changed outputStream.write

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,20 +4,20 @@ const outputStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 let readline = require('readline');
 let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 function handle() {
-    rl.output.write('Заходи ещё\n');
-    rl.close();
+  rl.output.write('Заходи ещё\n');
+  rl.close();
 }
 
 rl.output.write('Привет, напиши что-нибудь\n');
 rl.on('line', (input) => {
-    if (input === 'exit')
-        return handle();
-    outputStream.write(input);
+  if (input === 'exit')
+    return handle();
+  outputStream.write(`${input}\n`);
 }).on('SIGINT', handle);
 
 process.on('SIGINT', handle);
